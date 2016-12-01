aee4789271f81940a2b37749eb98e4c1eebc47fc
Amir Koklan <amirkoklan@gmail.com>
2016-12-01 01:57:17 +0100

    first commit

---
+++ src/AppBundle/Controller/ApplicationController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use eZ\Publish\API\Repository\Values\Content\Content;
+use eZ\Publish\Core\MVC\Symfony\View\ContentView;
+use eZ\Bundle\EzPublishCoreBundle\Controller;
+use eZ\Publish\API\Repository\Repository;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\Values\Content\Location;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApplicationController extends Controller {
+
+    private $repository;
+
+    public function __construct(Repository $repository) {
+        $this->repository = $repository;
+    }
+    public function viewLocationAction() {
+        
+        $searchService = $this->repository->getSearchService();
+        $locationService = $this->repository->getLocationService();
+        $contentService = $this->repository->getContentService();
+        $contentTypeService = $this->repository->getContentTypeService();
+
+                
+        $query = new Query(
+            array(
+                'filter' => new Criterion\LogicalAnd(
+                    array(
+                        new Criterion\ParentLocationId(54),
+                        new Criterion\Visibility(Criterion\Visibility::VISIBLE),
+                        new Criterion\ContentTypeIdentifier('application')
+                    )
+                )
+            )
+        );
+       $contentinfos = $searchService->findContentInfo($query);
+        $locations = [];
+        foreach ($contentinfos->searchHits as $result) {
+            $locations[] = $locationService->loadLocations($result->valueObject)[0];
+        }
+        print_r(count($locations));
+        if(!$locations){
+            $contentType = $contentTypeService->loadContentTypeByIdentifier( 'application' );
+            $contentCreateStruct = $contentService->newContentCreateStruct( $contentType , 'ger-DE');
+            $contentCreateStruct->setField( 'name', 'My title' );
+            $locationCreateStruct = $locationService->newLocationCreateStruct( 54 );
+
+            $draft = $contentService->createContent( $contentCreateStruct, array( $locationCreateStruct ) );
+            $content = $contentService->publishVersion( $draft->versionInfo );   
+            return new Response($content->id);
+        }
+        
+        return new Response($locations[0]->id);
+    }
+
+}
---
+++ src/AppBundle/QueryType/ChildrenQueryType.php
@@ -0,0 +1,65 @@
+<?php
+namespace AppBundle\QueryType;
+use eZ\Publish\Core\QueryType\QueryType;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\LocationQuery;
+class ChildrenQueryType implements QueryType
+{
+    public function getQuery(array $parameters = [])
+    {
+        $options = [];
+        $criteria = [
+            new Query\Criterion\ParentLocationId($parameters['parent_location_id']),
+            new Query\Criterion\Visibility(Query\Criterion\Visibility::VISIBLE),
+        ];
+        if (isset($parameters['excluded_content_types'])) {
+            $criteria[] = new Query\Criterion\LogicalNot(
+                new Query\Criterion\ContentTypeIdentifier($parameters['excluded_content_types'])
+            );
+        }
+        $options['filter'] = new Query\Criterion\LogicalAnd($criteria);
+        $sortClauses = new Query\SortClause\Location\Priority(Query::SORT_ASC);
+        if (isset($parameters['sorting_content_type']) &&
+            isset($parameters['sorting_field']) &&
+            isset($parameters['sorting_order'])) {
+            $sortClauses = new Query\SortClause\Field(
+                $parameters['sorting_content_type'],
+                $parameters['sorting_field'],
+                $parameters['sorting_order'] == 'desc' ? Query::SORT_DESC : Query::SORT_ASC
+            );
+        }
+        $options['sortClauses'] = [$sortClauses];
+        if (isset($parameters['limit'])) {
+            $options['limit'] = $parameters['limit'];
+        }
+        if (isset($parameters['offset'])) {
+            $options['offset'] = $parameters['offset'];
+        }
+        return new LocationQuery($options);
+    }
+    public static function getName()
+    {
+        return 'AppBundle:Children';
+    }
+    /**
+     * Returns array of required parameters.
+     *
+     * Optional parameters are:
+     *      - limit (int)
+     *      - offset (int)
+     *      - excluded_content_types (array|string)
+     *
+     * Additional parameters for sorting options (all of them are required):
+     *      - sorting_content_type (string)
+     *      - sorting_field (string)
+     *      - sorting_order (asc|desc)
+     *
+     * @return array
+     */
+    public function getSupportedParameters()
+    {
+        return [
+            'parent_location_id',
+        ];
+    }
+}
